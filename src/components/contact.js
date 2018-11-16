ad0b9527ac95749fe467d308ff6aa08b8d6f8738
Jeff S <Slater.Jeff7@gmail.com>
2018-11-16 14:28:22 -0700

    walkins

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,7 +9,8 @@ class Contact extends Component {
         <Col s={1} m={3} l={4}></Col>
         <Col s={10} m={6} l={4}>
           <h4 className="center-align">Contact</h4>
-          <p className="center-align info">*Available by appointments only*</p>
+          <p className="center-align info">*Walk-ins welcome, with availability*</p>
+          <p className="center-align info">*Call-In appointments prefered.*</p>
           <CardPanel className="lighten-4 white-text center-align">
             <h6><a href="tel: (303) 652-9088">(303) 652-9088</a></h6>
           </CardPanel>
